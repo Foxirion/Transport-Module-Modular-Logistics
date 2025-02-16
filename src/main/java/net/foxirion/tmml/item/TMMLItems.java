5b8093ab37231e05630fba8c07a94c168d1794fa
Foxirion <dariustam@yahoo.com>
2025-02-16 19:27:47 +0200

    1.21.4 - 3.0.0

--- src/main/java/net/foxirion/tmml/item/TMMLItems.java
+++ src/main/java/net/foxirion/tmml/item/TMMLItems.java
@@ -1,10 +1,7 @@
 package net.foxirion.tmml.item;
 
 import net.foxirion.tmml.init.TMML;
-import net.foxirion.tmml.item.custom.BlockTransportModule;
-import net.foxirion.tmml.item.custom.EntityTransportModule;
-import net.foxirion.tmml.item.custom.FluidTransportModule;
-import net.foxirion.tmml.item.custom.VoidBottleItem;
+import net.foxirion.tmml.item.custom.*;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.BlockItem;
